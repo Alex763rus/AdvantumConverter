68a2e93de0d11e7a2c323e3a0e09104425aa6b11
Григорьев Алексей Павлович <grigorevap@vtb.ru>
2025-03-08 22:44:44 +0400

    конвертер кофикса перевведен на v2

--- src/main/java/com/example/advantumconverter/service/excel/converter/client/ConvertServiceImplCofix.java
+++ src/main/java/com/example/advantumconverter/service/excel/converter/client/ConvertServiceImplCofix.java
@@ -2,9 +2,13 @@ package com.example.advantumconverter.service.excel.converter.client;
 
 import com.example.advantumconverter.enums.ExcelType;
 import com.example.advantumconverter.exception.ConvertProcessingException;
+import com.example.advantumconverter.exception.SberAddressNotFoundException;
+import com.example.advantumconverter.exception.TemperatureNodValidException;
 import com.example.advantumconverter.model.dictionary.excel.Header;
 import com.example.advantumconverter.model.jpa.Car;
-import com.example.advantumconverter.model.pojo.converter.ConvertedBook;
+import com.example.advantumconverter.model.pojo.converter.v2.ConvertedBookV2;
+import com.example.advantumconverter.model.pojo.converter.v2.ConvertedListDataV2;
+import com.example.advantumconverter.model.pojo.converter.v2.ConvertedListV2;
 import com.example.advantumconverter.service.excel.converter.ConvertService;
 import com.example.advantumconverter.service.excel.converter.ConvertServiceBase;
 import lombok.val;
@@ -14,17 +18,19 @@ import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.example.advantumconverter.constant.Constant.Command.COMMAND_CONVERT_COFIX;
 import static com.example.advantumconverter.constant.Constant.Converter.*;
 import static com.example.advantumconverter.constant.Constant.Exceptions.EXCEL_LINE_CONVERT_ERROR;
 import static com.example.advantumconverter.constant.Constant.FileOutputName.FILE_NAME_COFIX;
+import static com.example.advantumconverter.constant.Constant.Heap.*;
 import static com.example.advantumconverter.enums.ExcelType.CLIENT;
 import static org.example.tgcommons.constant.Constant.TextConstants.EMPTY;
 import static org.example.tgcommons.constant.Constant.TextConstants.SPACE;
 import static org.example.tgcommons.utils.DateConverterUtils.*;
-import static com.example.advantumconverter.constant.Constant.Heap.*;
 
 @Component
 public class ConvertServiceImplCofix extends ConvertServiceBase implements ConvertService {
@@ -43,61 +49,86 @@ public class ConvertServiceImplCofix extends ConvertServiceBase implements Conve
         return COMMAND_CONVERT_COFIX;
     }
 
+
+    private List<String> warnings = new ArrayList<>();
+
     @Override
-    public ConvertedBook getConvertedBook(XSSFWorkbook book) {
-        val data = new ArrayList<List<String>>();
-        data.add(Header.headersOutputClient);
+    public ConvertedBookV2 getConvertedBookV2(XSSFWorkbook book) {
+        warnings = new ArrayList<>();
+        val data = new ArrayList<ConvertedListDataV2>();
+        ConvertedListDataV2 dataLine = null;
+
         int row = START_ROW;
-        ArrayList<String> dataLine = new ArrayList();
         try {
             sheet = book.getSheetAt(0);
             LAST_ROW = getLastRow(START_ROW);
             LAST_COLUMN_NUMBER = sheet.getRow(START_ROW).getLastCellNum();
             for (; row < LAST_ROW; ++row) {
-                dataLine = new ArrayList<String>();
-                dataLine.add(fillA(row));
-                dataLine.add(convertDateFormat(getCellValue(1, 0), TEMPLATE_DATE_SLASH, TEMPLATE_DATE_DOT));
-                dataLine.add(FILE_NAME_COFIX);
-                dataLine.add(BUSH_AUTOPROM_ORGANIZATION_NAME);
-                dataLine.add(EMPTY);
-                dataLine.add(REFRIGERATOR);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(String.valueOf(findCar(row).getTonnage()));
-                dataLine.add(String.valueOf(findCar(row).getPallet()));
-                dataLine.add("2");
-                dataLine.add("4");
-                dataLine.add("6");
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(fillS(row));
-                dataLine.add(fillT(row));
-                dataLine.add(fillU(row));
-                dataLine.add(fillV(row));
-                dataLine.add(isStart(row) ? LOAD_THE_GOODS : UNLOAD_THE_GOODS);
-                dataLine.add(fillX(row));
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(fillAC(row));
-                dataLine.add(EMPTY);
-                dataLine.add(fillAE(row));
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
-                dataLine.add(EMPTY);
 
+                dataLine = ConvertedListDataV2.init()
+                        .setColumnAdata(fillA(row))
+                        .setColumnBdata(convertDateFormat(getCellValue(1, 0), TEMPLATE_DATE_SLASH))
+                        .setColumnCdata(FILE_NAME_COFIX)
+                        .setColumnDdata(BUSH_AUTOPROM_ORGANIZATION_NAME)
+                        .setColumnEdata(null)
+                        .setColumnFdata(REFRIGERATOR)
+                        .setColumnGdata(EMPTY)
+                        .setColumnHdata(EMPTY)
+                        .setColumnIdata(null)
+                        .setColumnJdata(findCar(row).getTonnage())
+                        .setColumnKdata(findCar(row).getPallet())
+                        .setColumnLdata(2)
+                        .setColumnMdata(4)
+                        .setColumnNdata(6)
+                        .setColumnOdata(null)
+                        .setColumnPdata(null)
+                        .setColumnQdata(null)
+                        .setColumnRdata(null)
+                        .setColumnSdata(fillS(row))
+                        .setColumnTdata(fillT(row))
+                        .setColumnUdata(fillU(row))
+                        .setColumnVdata(fillV(row))
+                        .setColumnWdata(isStart(row) ? LOAD_THE_GOODS : UNLOAD_THE_GOODS)
+                        .setColumnXdata(fillX(row))
+                        .setColumnYdata(null)
+                        .setColumnZdata(null)
+                        .setColumnAaData(EMPTY)
+                        .setColumnAbData(EMPTY)
+                        .setColumnAcData(fillAC(row))
+                        .setColumnAdData(EMPTY)
+                        .setColumnAeData(fillAE(row))
+                        .setColumnAfData(null)
+                        .setColumnAgData(EMPTY)
+                        .setColumnAhData(EMPTY)
+                        .setColumnAiData(EMPTY)
+                        .setColumnAjData(EMPTY)
+                        .setColumnAkData(EMPTY)
+                        .setColumnAlData(EMPTY)
+                        .setColumnAmData(EMPTY)
+                        .setColumnAnData(EMPTY)
+                        .setColumnAoData(EMPTY)
+                        .setColumnApData(null)
+                        .setTechFullFio(null)
+                        .build();
                 data.add(dataLine);
             }
+        } catch (SberAddressNotFoundException | TemperatureNodValidException e) {
+            throw e;
         } catch (Exception e) {
             throw new ConvertProcessingException(String.format(EXCEL_LINE_CONVERT_ERROR, row, dataLine, e.getMessage()));
         }
-        return createDefaultBook(getConverterName() + UNDERSCORE, EXPORT, data, DONE);
+        return ConvertedBookV2.init()
+                .setBookV2(List.of(
+                        ConvertedListV2.init()
+                                .setHeadersV2(Header.headersOutputClientV2)
+                                .setExcelListName(EXPORT)
+                                .setExcelListContentV2(data)
+                                .build()
+                ))
+                .setMessage(DONE + warnings.stream().distinct().collect(Collectors.joining("")))
+                .setBookName(getConverterName() + UNDERSCORE)
+                .build();
     }
-
 
     private String fillA(int row) throws ParseException {
         val cur = getValueOrDefault(row, 0, 3);
@@ -115,16 +146,15 @@ public class ConvertServiceImplCofix extends ConvertServiceBase implements Conve
         return dictionaryService.getCar(carName);
     }
 
-    private String fillS(int row) throws ParseException {
+    private Date fillS(int row) throws ParseException {
         val next1 = getValueOrDefault(row, 1, 0);
         String dateResult = getCellValue(1, 0) + SPACE + (isStart(row) ? next1 : getCellValue(row, 5));
-        return convertDateFormat(dateResult, TEMPLATE_DATE_TIME_SLASH, TEMPLATE_DATE_TIME_DOT);
+        return convertDateFormat(dateResult, TEMPLATE_DATE_TIME_SLASH);
     }
 
-    private String fillT(int row) throws ParseException {
-        val dateText = fillS(row);
-        val date = convertDateFormat(dateText, TEMPLATE_DATE_TIME_DOT);
-        return convertDateFormat(DateUtils.addHours(date, 1), TEMPLATE_DATE_TIME_DOT);
+    private Date fillT(int row) throws ParseException {
+        val dateS = fillS(row);
+        return DateUtils.addHours(dateS, 1);
     }
 
     private String fillU(int row) {
@@ -135,7 +165,7 @@ public class ConvertServiceImplCofix extends ConvertServiceBase implements Conve
         return isStart(row) ? fillU(row) : getCellValue(row, 11);
     }
 
-    private String fillX(int row) {
+    private Integer fillX(int row) {
         int i = row;
         for (; i >= START_ROW; --i) {
             if (isStart(i)) {
@@ -145,7 +175,7 @@ public class ConvertServiceImplCofix extends ConvertServiceBase implements Conve
                 break;
             }
         }
-        return String.valueOf(row - i + 1);
+        return row - i + 1;
     }
 
     private String fillAC(int row) {
