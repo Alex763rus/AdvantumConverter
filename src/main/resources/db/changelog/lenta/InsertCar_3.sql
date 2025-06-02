3e0dd453549a46933681588d276d281c65813800
Григорьев Алексей Павлович <grigorevap@vtb.ru>
2025-06-02 22:17:51 +0400

    добавлены машины ленты

---
+++ src/main/resources/db/changelog/lenta/InsertCar_3.sql
@@ -0,0 +1,14 @@
+insert into car(car_name, pallet, tonnage, temperature_min, temperature_max)
+select 'ТЕРМО_4_5_ТОНН_ГИДРОБОРТ', 77777, 4500, 2, 5 union all
+select 'РЕФ_20_ТОНН_1_C', 77777, 20000, 1, 5 union all
+select 'РЕФРЕЖИРАТОРНЫЕ 20Т', 77777, 20000, 2, 5 union all
+select 'РЕФ_10_ТОНН_-18_C', 77777, 10000, -18, 5 union all
+select 'РЕФ_5_ТОНН_4_C', 77777, 5000, 4, 5
+;
+
+
+
+
+
+
+
