0dbdec27b11a9f9b3c726c483864d85eac5b9637
Григорьев Алексей Павлович <grigorevap@vtb.ru>
2025-11-26 14:01:53 +0400

    добавлен новый пользователь Ольга

---
+++ src/main/resources/db/changelog/users/InsertUser5.sql
@@ -0,0 +1,3 @@
+replace into user_tbl(chat_id, first_name, last_name, registered_at, user_name, user_role, company_id, user_role_text)
+select '628829464', 'Ольга Мещанина', '', '2025-11-26 14:35:14.851000', 'OlgaM77887u', 7, 3, 'EMPLOYEE_RS'
+;
