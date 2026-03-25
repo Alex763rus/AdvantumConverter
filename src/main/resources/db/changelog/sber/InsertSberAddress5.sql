f60fa0220d9b6c93adafc310dc391703f9c0b760
Григорьев Алексей Павлович <grigorevap@vtb.ru>
2026-03-25 22:32:00 +0400

    добавлен адрес сбера

---
+++ src/main/resources/db/changelog/sber/InsertSberAddress5.sql
@@ -0,0 +1,5 @@
+insert into sber_address(city, address, city_and_region)
+select 'Москва',
+       'Российская Федерация, Ивановская область, г. Иваново, ул. 2-я Сельская, д 2А',
+       'Москва'
+;
