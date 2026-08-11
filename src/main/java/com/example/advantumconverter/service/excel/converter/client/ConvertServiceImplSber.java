70d7f86e4bbf472ea55f1b11d0096a536ac87b32
Григорьев Алексей Павлович <grigorevap@vtb.ru>
2026-08-11 08:55:49 +0400

    дата в конвертере сбера принимается с точками

--- src/main/java/com/example/advantumconverter/service/excel/converter/client/ConvertServiceImplSber.java
+++ src/main/java/com/example/advantumconverter/service/excel/converter/client/ConvertServiceImplSber.java
@@ -277,7 +277,7 @@ public class ConvertServiceImplSber extends ConvertServiceBase implements Conver
 
     @SneakyThrows
     private Date getDateFromFile(int row) {
-        return convertDateFormat(getCellValue(row, 16), TEMPLATE_DATE_SLASH);
+        return convertDateFormat(getCellValue(row, 16).replaceAll("\\.", "/"), TEMPLATE_DATE_SLASH);
     }
 
     @Override
